b35dc33371511bf2fafb926003d24d9198d88be3
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-01-25 15:52:22 +0800

    Homework 6

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,13 +1,8 @@
-// Select the ul#categories element
 const categoriesList = document.getElementById('categories');
-
-// Get all li.item elements within ul#categories
 const categories = categoriesList.querySelectorAll('li.item');
-
-// Display the number of categories
 console.log(`Number of categories: ${categories.length}\n`);
 
-// Loop through each category and display header text and the number of elements
+
 categories.forEach(category => {
     const categoryName = category.querySelector('h2').textContent;
     const categoryElements = category.querySelectorAll('ul li').length;
